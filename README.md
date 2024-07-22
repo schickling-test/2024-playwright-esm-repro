5590a9f5a139dce7899ebc541c2e03f68cfaf5e3
Johannes Schickling <schickling.j@gmail.com>
2024-07-22 18:11:13 +0200

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Repro
+
+```
+pnpm install
+pnpm build:ts
+
+cd packages/@acme/tests
+pnpm test
+```
--- packages/@acme/tests/src/hello.test.ts
+++ packages/@acme/tests/src/hello.test.ts
@@ -1,5 +1,5 @@
-import { greet } from '@acme/lib' // doesn't work
-// import { greet} from '@acme/lib/dist/index.js' // works
+// import { greet } from '@acme/lib' // doesn't work
+import { greet} from '@acme/lib/dist/index.js' // works
 import { test } from '@playwright/test'
 
 test(
